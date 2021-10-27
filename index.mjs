ceac35ffffa7e7f9327f70d0e11eeb9245b22608
catalinpit <catalinpit@gmail.com>
2021-10-27 17:41:52 +0300

    Add a new resource

--- index.mjs
+++ index.mjs
@@ -3,12 +3,9 @@ import express, { response } from 'express';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy'
-    }
-];
+app.use(express.json());
+
+let notes = [];
 
 app.get('/', (req, res) => {
     res.send('Notes API');
@@ -27,6 +24,35 @@ app.get('/api/notes/:id', (req, res) => {
     } else {
         res.status(404).end();
     }
+});
+
+app.post('/api/notes', (req, res) => {
+    // not the recommended way to do this
+    // change asap
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0;
+
+    const body = req.body;
+
+    if (!body.content) {
+        return res.status(400).json({
+            error: 'missing content'
+        });
+    }
+
+    console.log(body);
+
+    const note = {
+        id: maxId + 1,
+        data: new Date(),
+        content: body.content,
+        important: body.important
+    }
+
+    notes = notes.concat(note);
+
+    res.json(note);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
