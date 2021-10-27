58fc0b74dac9151f67bb2107851bb1b6238cf78d
catalinpit <catalinpit@gmail.com>
2021-10-27 17:18:59 +0300

    fetch a single resource

--- index.mjs
+++ index.mjs
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-let notes = [];
+let notes = [
+    {
+        id: 1,
+        content: 'HTML is easy'
+    }
+];
 
 app.get('/', (req, res) => {
     res.send('Notes API');
@@ -11,6 +16,17 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id);
+
+    if (note) {
+        res.json(note);
+    } else {
+        res.status(404).end();
+    }
 });
 
 app.listen(PORT, () => {
