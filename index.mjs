9a88c93ffff6459754ca25083135cc64c5824735
Catalin Pit <catalinpit@gmail.com>
2021-11-20 14:40:02 +0200

    Use MongoDB to retrieve a note

--- index.mjs
+++ index.mjs
@@ -32,14 +32,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
-        res.json(note);
-    } else {
-        res.status(404).end();
-    }
+    Note.findById(req.params.id)
+    .then(note => {
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(400).send({ error: 'Malformatted id' })
+    });
 });
 
 app.post('/api/notes', (req, res) => {
@@ -64,9 +67,6 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    res.status(204).end();
 });
 
 const unknownEndpoint = (req, res) => {
